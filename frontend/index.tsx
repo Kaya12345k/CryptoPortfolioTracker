2fb79e151449b88115c96a4d1478ce6fd154f496
Kaya12345k <72180342+Kaya12345k@users.noreply.github.com>
2024-05-30 11:25:17 +0300

    optimization improvement

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -1,9 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+No code snippet was provided in the text.
